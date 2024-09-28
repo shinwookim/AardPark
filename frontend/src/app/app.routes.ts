8ac127390bff11db94620336fcfc35b982909d1e
Cyanox62 <18158351+Cyanox62@users.noreply.github.com>
2024-09-28 13:22:04 -0400

    added default navigation to /home

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 export const routes: Routes = [
 	// Redirect root page to home
-	//{ path: '', redirectTo: '/home', pathMatch: 'full' },
+	{ path: '', redirectTo: '/home', pathMatch: 'full' },
 	// Component routes
 	{ path: 'home', component: HomeComponent },
 	{ path: 'sign-up', component: SignUpComponent },
