437a2712ed9f76d06e030b0e591e42ba4e41c1af
Shinwoo Kim <shinwookim@pitt.edu>
2024-09-28 16:25:06 -0400

    feat: parking_spots and users API

---
+++ backend/aardpark/database.py
@@ -0,0 +1,19 @@
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()  # take environment variables from .env.
+
+# configure SQLAlchemy
+uri = f'mongodb+srv://{getenv("MONGODB_USERNAME")}:{getenv("MONGODB_PASSWORD")}@{getenv("MONGODB_CONNECTION")}'
+client = MongoClient(uri, server_api=ServerApi("1"))
+db = client["mydatabase"]
+
+# MongoDB Collection Names
+User = db["User"]
+ParkingSpot = db["ParkingSpot"]
+ChatMessage = db["ChatMessage"]
+Booking = db["Booking"]
+Availability = db["Availability"]
--- backend/aardpark/routers/users.py
+++ backend/aardpark/routers/users.py
@@ -1,23 +1,52 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, Path, Query
+from aardpark.database import User
+from pymongo.results import InsertOneResult
+from werkzeug.security import check_password_hash, generate_password_hash
+
 
 router = APIRouter()
 
 
 @router.post("/login")
 def login():
+    """
+    This is a login endpoint.
+    """
     pass
 
 
 @router.get("/logout")
 def logout():
+    """
+    This is a logout endpoint.
+    """
     pass
 
 
-@router.get("/users/{user_id}")
-def get_user():
+@router.get("/users/{user_email}")
+def get_user(
+    user_email: Annotated[
+        str, Path(description="User email address, which also serve as their username")
+    ],
+):
+    """
+    Returns the user information.
+    """
     pass
 
 
 @router.post("/users/")
-def new_user():
-    pass
+def new_user(
+    name: Annotated[str, Query(description="The name of the user. E.g., John Doe")],
+    email: Annotated[str, Query(description="The email address of the user")],
+    password: Annotated[str, Query(description="The password of the user")],
+):
+    document: InsertOneResult = User.insert_one(
+        {
+            "name": name,
+            "email": email,
+            "password": generate_password_hash(password),
+        }
+    )
+    return {"id": str(document.inserted_id), "acknowledged": document.acknowledged}
