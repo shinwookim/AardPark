7c3ff00a70437f0fb2170b307afd58f4434083cb
Cyanox62 <18158351+Cyanox62@users.noreply.github.com>
2024-09-28 13:04:39 -0400

    Added proper layout structure

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 export const routes: Routes = [
 	// Redirect root page to home
-	{ path: '', redirectTo: '/home', pathMatch: 'full' },
+	//{ path: '', redirectTo: '/home', pathMatch: 'full' },
 	// Component routes
 	//{ path: 'home', component: HomeComponent },
+	{ path: 'sign-up', component: SignUpComponent },
 ];
