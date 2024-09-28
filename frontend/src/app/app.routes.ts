46f597b1085585347c4d32abadc88f002e93b143
Cyanox62 <18158351+Cyanox62@users.noreply.github.com>
2024-09-28 11:57:33 -0400

    basic layout finished

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	// Redirect root page to home
+	{ path: '', redirectTo: '/home', pathMatch: 'full' },
+	// Component routes
+	//{ path: 'home', component: HomeComponent },
+];
