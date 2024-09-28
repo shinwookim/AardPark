9cd2fd6119b530658b1e81404d5782b00371003d
Shinwoo Kim <shinwookim@pitt.edu>
2024-09-28 19:14:57 -0400

    feat: auth0 interface

--- backend/aardpark/main.py
+++ backend/aardpark/main.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
 from .routers import availability, bookings, messages, parking_spots, users
+from .auth0 import Auth0
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()  # take environment variables from .env.
+
+AUTH_ZERO_API_URL = getenv("AUTH_ZERO_API_URL")
+auth0 = Auth0(AUTH_ZERO_API_URL)
 
 app = FastAPI()
 app.include_router(availability.router)
