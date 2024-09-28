70c4927494a40e62c60fe0f74e7f090e0a5a3e54
Cyanox62 <18158351+Cyanox62@users.noreply.github.com>
2024-09-28 15:35:08 -0400

    login page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
 	// Redirect root page to home
@@ -8,4 +9,5 @@ export const routes: Routes = [
 	// Component routes
 	{ path: 'home', component: HomeComponent },
 	{ path: 'sign-up', component: SignUpComponent },
+	{ path: 'login', component: LoginComponent }
 ];
