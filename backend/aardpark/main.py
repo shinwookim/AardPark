45e4496742dafc79a454e50c67cbaa93b3c5a497
Shinwoo Kim <shinwookim@pitt.edu>
2024-09-28 19:32:35 -0400

    feat: chatting hehe

--- backend/aardpark/main.py
+++ backend/aardpark/main.py
@@ -1,13 +1,5 @@
 from fastapi import FastAPI
-from .routers import availability, bookings, messages, parking_spots, users
-from .auth0 import Auth0
-from dotenv import load_dotenv
-from os import getenv
-
-load_dotenv()  # take environment variables from .env.
-
-AUTH_ZERO_API_URL = getenv("AUTH_ZERO_API_URL")
-auth0 = Auth0(AUTH_ZERO_API_URL)
+from .routers import availability, bookings, messages, parking_spots
 
 app = FastAPI()
 app.include_router(availability.router)
