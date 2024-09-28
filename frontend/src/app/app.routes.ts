59eed10698a0d8badad471a6120a760adcb11da4
uboaaaa <mik172@pitt.edu>
2024-09-28 17:31:15 -0400

    added a listings page with dummy data

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { LoginComponent } from './components/login/login.component';
+import { ListingsComponent } from './components/listings/listings.component';
 
 export const routes: Routes = [
 	// Redirect root page to home
 	{ path: '', redirectTo: '/home', pathMatch: 'full' },
 	// Component routes
 	{ path: 'home', component: HomeComponent },
+	{ path: 'listings', component: ListingsComponent },
 	{ path: 'sign-up', component: SignUpComponent },
 	{ path: 'login', component: LoginComponent }
 ];
