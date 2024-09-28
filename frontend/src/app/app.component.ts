741c0da1a3ed52f95f5469febe33737bb1a13424
uboaaaa <mik172@pitt.edu>
2024-09-28 13:14:04 -0400

    landing page WIP

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './layout/header/header.component';
-import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
 	// Redirect root page to home
 	{ path: '', redirectTo: '/home', pathMatch: 'full' },
 	// Component routes
-	//{ path: 'home', component: HomeComponent },
+	{ path: 'home', component: HomeComponent },
 ];
