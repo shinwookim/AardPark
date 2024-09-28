b406c1c40f26b369feb339a410d5a83ba09210be
Cyanox62 <18158351+Cyanox62@users.noreply.github.com>
2024-09-28 19:30:31 -0400

    addded auth0 login

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,7 +2,25 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAuth0 } from '@auth0/auth0-angular';
+import { IMAGE_CONFIG } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideAuth0({
+      domain: 'dev-706qhrvchpeatoc8.us.auth0.com',
+      clientId: 'grYiZAlXeaYe3uVdXwtgu96M562AMu98',
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }
+    }),
+		{
+			provide: IMAGE_CONFIG,
+			useValue: {
+				disableImageSizeWarning: true, 
+				disableImageLazyLoadWarning: true
+			}
+		},
+  ]
 };
