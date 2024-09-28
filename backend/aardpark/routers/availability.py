5a1c57ba0d9674f0dbf74fab956bdd186b421353
Maanya Shanker <maanyashanker30@gmail.com>
2024-09-28 17:16:10 -0400

    feature: added mongodb stuff for availability and bookings

--- backend/aardpark/routers/availability.py
+++ backend/aardpark/routers/availability.py
@@ -1,24 +1,32 @@
-from fastapi import APIRouter
-from backend.aardpark.database import Availability
-from pymango import ObjectId
+from fastapi import APIRouter, Query, Path
+from typing import Annotated
+from aardpark.database import Availability
+import json
+from bson import json_util
 
 router = APIRouter()
 
-
 @router.get("/availability/{spot_id}")
-def get_availability(spot_id: str):
-    myquery = {"parking_spot": ObjectId(spot_id)}
+def get_availability(spot_id: Annotated[str, Path(description="The ID of the parking spot to query.")]):
+    myquery = {"parking_spot": spot_id}
     query_result = Availability.find(myquery)
-    return list(query_result)
+    
+    response = json.loads(json_util.dumps(query_result))
+    return response
 
 
 @router.post("/availability")
-def new_availability(parking_spot: str, start_time: str, end_time: str):
-    # Add category item
+def new_availability(parking_spot: Annotated[str, Query(description="The ID of the parking spot to add to the availability.")], 
+                     start_time: Annotated[str, Query(description="The start time of the availability.")], 
+                     end_time: Annotated[str, Query(description="The end time of the availability.")], 
+                     latitude: Annotated[float, Query(description="The latitude of the spot.")], 
+                     longitude: Annotated[float, Query(description="The latitude of the spot.")]):
+    # Add item
     new_parking = {
         "parking_spot": parking_spot, 
         "start_time": start_time, 
-        "end_time": end_time
+        "end_time": end_time,
+        "location": {"type": "Point", "coordinates": [latitude, longitude]},
     }
     
     Availability.insert_one(new_parking)
--- backend/aardpark/routers/bookings.py
+++ backend/aardpark/routers/bookings.py
@@ -1,18 +1,50 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, Query, Path
+from typing import Annotated
+from aardpark.database import Booking
+import json
+from bson import json_util
+from bson.json_util import loads, dumps
 router = APIRouter()
+from pydantic import BaseModel
 
+class Bookings(BaseModel):
+    purchasher: str
+    seller: str
+    parking_spot: str
+    start_time: str
+    end_time: str
 
 @router.get("/booking/")
 def get_all_bookings():
-    pass
+    query_result = Booking.find()
+    
+    response = json.loads(json_util.dumps(query_result))
+    return response
 
 
 @router.post("/booking/")
-def new_booking():
-    pass
-
+def new_booking(purchaser: Annotated[str, Query(description="The user who is purchasing the spot")],
+                seller: Annotated[str, Query(description="The user who is purchasing the spot")],
+                parking_spot: Annotated[str, Query(description="The user who is purchasing the spot")],
+                start_time: Annotated[str, Query(description="The user who is purchasing the spot")],
+                end_time: Annotated[str, Query(description="The user who is purchasing the spot")]):
+    # Add item
+    new_booking = {
+        "parking_spot": parking_spot, 
+        "start_time": start_time, 
+        "end_time": end_time,
+        "purchaser": purchaser,
+        "seller": seller
+    }
+    
+    Booking.insert_one(new_booking)
 
 @router.get("/booking/{user_id}")
-def get_individual_booking():
-    pass
+def get_individual_booking(user_id: Annotated[str, Path(description="The user id to see purchases and sellers.")]):
+    purchaser = Booking.find({"purchaser": user_id}, {"_id":0})
+    seller = Booking.find({"seller": user_id}, {"_id":0})
+
+    response = {"purchaser": list(purchaser), "seller": list(seller)}
+    # response = [dict_data for id, dict_data in purchaser]
+    # print(response["purchaser"]["parking_spot"])
+    return response
