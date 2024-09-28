c0fd23771a413ed363850923c0d4aae317783b97
Maanya Shanker <maanyashanker30@gmail.com>
2024-09-28 15:26:45 -0400

    feature: availability endpoint done (not tested)

--- backend/aardpark/routers/availability.py
+++ backend/aardpark/routers/availability.py
@@ -1,13 +1,24 @@
 from fastapi import APIRouter
+from backend.aardpark.database import Availability
+from pymango import ObjectId
 
 router = APIRouter()
 
 
 @router.get("/availability/{spot_id}")
-def get_availability(spot_id: int):
-    pass
+def get_availability(spot_id: str):
+    myquery = {"parking_spot": ObjectId(spot_id)}
+    query_result = Availability.find(myquery)
+    return list(query_result)
 
 
 @router.post("/availability")
-def new_availability():
-    pass
+def new_availability(parking_spot: str, start_time: str, end_time: str):
+    # Add category item
+    new_parking = {
+        "parking_spot": parking_spot, 
+        "start_time": start_time, 
+        "end_time": end_time
+    }
+    
+    Availability.insert_one(new_parking)
