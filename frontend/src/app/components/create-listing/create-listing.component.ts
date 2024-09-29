9e938f213baf298c8c0a243c674c50c18564ca0b
Maanya Shanker <maanyashanker30@gmail.com>
2024-09-29 04:22:33 -0400

    feature: create listing integration (?)

--- frontend/src/app/components/create-listing/create-listing.component.ts
+++ frontend/src/app/components/create-listing/create-listing.component.ts
@@ -1,22 +1,34 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../../services/api.service';
 import { GeocodeLocation } from '../../data-classes/GeocodeLocation';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-create-listing',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './create-listing.component.html',
   styleUrl: './create-listing.component.css'
 })
 export class CreateListingComponent {
-	constructor(private apiService: ApiService) {}
+	title: string = "";
+	location: string = "";
+	description: string = "";
+	price: string = "";
+	start: string = "";
+	end: string = "";
+
+	constructor(private apiService: ApiService, private auth: AuthService) {}
 	
 	createListing() {
-		this.apiService._getLatLonFromAddress("325 North Craig Street").subscribe({
+		this.apiService._getLatLonFromAddress(this.location).subscribe({
 			next: (data: GeocodeLocation) => {
 				console.log(data);
+				this.apiService._newParkingSpot(this.title, this.description, data.lat, data.lng, auth.email, this.start, this.end);
 			}
 		})
+
+
 	}
 }
